6b185cc689c427e69c92ee0003dd9b6dd0351295
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-09-20 12:02:11 +0900

    td require

--- lib/heroku/commands/td.rb
+++ lib/heroku/commands/td.rb
@@ -1,12 +1,11 @@
+require 'td'
+require 'td/command/td'
+
 module Heroku::Command
 
   class Td < BaseWithApp
     def index
       puts 'hello world!'
-    end
-
-    def db:create
-      puts 'comma test'
     end
   end
 
