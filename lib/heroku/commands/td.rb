c356fd45466e047e6cd0ed16918bdab450f9a8ee
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2011-09-20 15:21:25 +0900

    updated to use TreasureData::Command::Runner

--- lib/heroku/commands/td.rb
+++ lib/heroku/commands/td.rb
@@ -1,45 +1,22 @@
-require 'td'
-require 'td/command/list'
-require 'td/config'
+require 'td/command/runner'
 
 module Heroku::Command
 
   class Td < BaseWithApp
     def index
-      cmd = args.shift
-      unless cmd
-        puts "TODO: show usage"
-        return -1
+      r = TreasureData::Command::Runner.new
+      r.prog_name = "#{File.basename($0)} td"
+
+      if args.empty?
+        # show usage
+        r.run args
+        exit 0
       end
 
-      # modify ARGV for td
-      ARGV.clear
-      ARGV.concat(args)
+      # this does internet access
+      r.apikey = heroku.config_vars(app)['TREASURE_DATA_API_KEY']
 
-      # set apikey
-      apikey =  heroku.config_vars(app)['TREASURE_DATA_API_KEY']
-      TreasureData::Config.apikey = apikey
-
-      method = TreasureData::Command::List.get_method(cmd)
-      unless method
-        $stderr.puts "'#{cmd}' is not a td command. Run '#{$prog}' to show the list."
-        TreasureData::Command::List.show_guess(cmd)
-        exit 1
-      end
-
-      begin
-        method.call
-      rescue TreasureData::ConfigError
-        $stderr.puts "TreasureData account is not configured yet."
-        $stderr.puts "Run '#{$prog} account' first."
-      rescue
-        $stderr.puts "error #{$!.class}: backtrace:"
-        $!.backtrace.each {|b|
-          $stderr.puts "  #{b}"
-        }
-        puts ""
-        puts $!
-      end
+      r.run args
     end
   end
 
