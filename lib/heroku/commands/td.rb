16227d27381dbe3d46b06e7781a9b26788302123
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-09-20 12:08:55 +0900

    update

--- lib/heroku/commands/td.rb
+++ lib/heroku/commands/td.rb
@@ -12,13 +12,25 @@ module Heroku::Command
       end
 
       method = TreasureData::Command::List.get_method(cmd)
-      p method
       unless method
         $stderr.puts "'#{cmd}' is not a td command. Run '#{$prog}' to show the list."
         TreasureData::Command::List.show_guess(cmd)
         exit 1
       end
-      puts 'hello world!'
+
+      begin
+        method.call
+      rescue TreasureData::ConfigError
+        $stderr.puts "TreasureData account is not configured yet."
+        $stderr.puts "Run '#{$prog} account' first."
+      rescue
+        $stderr.puts "error #{$!.class}: backtrace:"
+        $!.backtrace.each {|b|
+          $stderr.puts "  #{b}"
+        }
+        puts ""
+        puts $!
+      end
     end
   end
 
