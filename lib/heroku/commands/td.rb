e07a60e66a74fbd19f074b6495f82ad2df322436
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-09-20 12:07:58 +0900

    update

--- lib/heroku/commands/td.rb
+++ lib/heroku/commands/td.rb
@@ -5,9 +5,19 @@ module Heroku::Command
 
   class Td < BaseWithApp
     def index
-      p args
       cmd = args.shift
-      p cmd
+      unless cmd
+        # TODO: show usage
+        return -1
+      end
+
+      method = TreasureData::Command::List.get_method(cmd)
+      p method
+      unless method
+        $stderr.puts "'#{cmd}' is not a td command. Run '#{$prog}' to show the list."
+        TreasureData::Command::List.show_guess(cmd)
+        exit 1
+      end
       puts 'hello world!'
     end
   end
