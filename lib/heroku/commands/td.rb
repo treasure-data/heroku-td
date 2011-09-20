4cbbb381c06a3c2e8a666998a014719a6d29e6a7
Kazuki Ohta <kazuki.ohta@gmail.com>
2011-09-20 11:46:21 +0900

    initial commit

---
+++ lib/heroku/commands/td.rb
@@ -0,0 +1,9 @@
+module Heroku::Command
+
+  class Td < BaseWithApp
+    def index
+      puts 'hello world!'
+    end
+  end
+
+end
---
+++ lib/heroku/td.rb
@@ -0,0 +1,8 @@
+# heroku gem version check
+if Heroku::VERSION < '2.0.0'
+  puts "Please upgrade your Heroku gem"
+elsif Heroku::VERSION >= '3.0.0'
+  puts "Please update your heroku-rds plugin"
+else
+  require 'heroku/commands/rds'
+end
