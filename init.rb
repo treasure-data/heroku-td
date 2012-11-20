1cbdef4884626c38b75a8a5fb25092607f1ba7a9
Kazuki Ohta <kazuki.ohta@gmail.com>
2012-11-20 13:43:39 -0800

    modify for upgrading td gem

--- init.rb
+++ init.rb
@@ -4,6 +4,17 @@ require 'heroku-api'
 # From Dec 2012, Heroku will deprecate `heroku` gem and recommend using Heroku
 # toolbelt. That required us to install `td` gem with `ruby` interpreter, which
 # is bundled with Heroku toolbelt.
+TD_VERSION = '0.10.61'
+
+def install_td_gem
+  begin
+    puts "Installing required gems for 'heroku td'..."
+    Gem::GemRunner.new.run ['install', 'td', '--no-ri', '--no-rdoc', '--version', '0.10.61', '--user-install']
+  rescue Gem::SystemExitException => e
+    exit e.exit_code
+  end
+end
+
 begin
   require 'rubygems' unless defined?(gem)
   require 'rubygems/gem_runner'
@@ -11,13 +22,12 @@ begin
   $LOAD_PATH.unshift(Gem.user_dir)
   require 'td/client'
   require 'td/command/runner'
-rescue LoadError
-  begin
-    puts "Installing required gems for 'heroku td'..."
-    Gem::GemRunner.new.run ['install', 'td', '--no-ri', '--no-rdoc', '--version', '0.10.61', '--user-install']
-  rescue Gem::SystemExitException => e
-    exit e.exit_code
+  require 'td/version'
+  if TreasureData::VERSION != TD_VERSION
+    install_td_gem()
   end
+rescue LoadError
+  install_td_gem()
 end
 
 begin
