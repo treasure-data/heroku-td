7a4edc22dfa38e04cf0558412fc47d0f99ed0547
Kazuki Ohta <kazuki.ohta@gmail.com>
2013-02-17 23:51:14 -0800

    warning for old heroku toolbelt

--- init.rb
+++ init.rb
@@ -1,3 +1,9 @@
-require 'heroku-api'
-require 'heroku/helpers'
+begin
+  require 'heroku-api'
+  require 'heroku/helpers'
+rescue LoadError => e
+  p e
+  $stderr.puts " !    Error: Your heroku toolbelt is too old. Please upgrade from here. > https://toolbelt.heroku.com/"
+  exit
+end
 require File.dirname(__FILE__) + '/lib/heroku/td'
